4e06c3091b48abb23aaaf9e7c2abbac32604b30a
efirmansyah <endarfirmansyah@gmail.com>
2020-10-16 11:59:23 +0700

    Pelatihan hari ke-4

---
+++ latihan04.py
@@ -0,0 +1,52 @@
+string = "Hello World"
+list = string.split(" ")
+print("Index untuk huruf" ,"l", "pada kata 'Hello World' adalah:") 
+print(string.index('l'))
+
+print("Count untuk huruf" ,"l", "pada kata 'Hello World' adalah:")
+print(string.count('l'))
+
+print(string[3:7])
+
+print(string[3:7:2])
+
+print(string[-11])
+
+print(string[::-1])
+
+
+print(string.upper())
+
+print(string.lower())
+
+print(len(string))
+
+
+afewwords = string.split(" ")
+
+if "Hello" in list:
+	print("Yes we have")
+
+
+x = [1,2,3]
+y = [1,2,3]
+
+print(x==y)
+
+print(x is y)
+
+print(x is x)
+
+#condition
+x = 2
+if x == 2:
+	print(x)
+
+if not x !=2:
+	print(x)
+
+
+
+
+
+
