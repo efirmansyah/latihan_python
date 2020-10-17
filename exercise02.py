7e6e39588fc261cbab069be6bd25c9ca3143acf8
efirmansyah <endarfirmansyah@gmail.com>
2020-10-17 15:14:36 +0700

    Exercise02

---
+++ exercise02.py
@@ -0,0 +1,43 @@
+# change this code
+number = 16
+
+second_number = 10
+
+first_array = [1, 2]
+
+second_array = [1, 2, 3]
+
+if number > 10:
+	print("1")
+else:
+	print('You must print 1')
+
+
+if first_array:
+	print("2")
+else:
+	print('You must print 2')
+
+
+if len(second_array) == 3:
+	print("3")
+else:
+	print('You must print 3')
+
+
+if len(first_array) + len(second_array) == 5:
+	print("4")
+else:
+	print('You must print 4')
+
+
+if first_array and first_array[0] == 1:
+	print("5")
+else:
+	print('You must print 5')
+
+
+if second_number:
+	print("6")
+else:
+	print('You must print 6') 
