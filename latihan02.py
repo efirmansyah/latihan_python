f74ee3f233569ac3744e16aa4d311bf28b291922
efirmansyah <endarfirmansyah@gmail.com>
2020-10-18 20:39:54 +0700

    Move file

---
+++ latihan02.py
@@ -0,0 +1,19 @@
+mylist = ['Luke Skywalke', 'Anakin Skywalker', 'Yoda', 'Padme Amidala']
+print(mylist)
+
+hello = "Hello" +""+ "World"
+print("Hello")
+
+print(mylist [:4])
+
+print(mylist [1:3])
+
+print(mylist [::-1])
+
+for index in mylist[::-1]:
+	print(index)
+
+modulo = 11 % 3
+print(modulo)
+
+
