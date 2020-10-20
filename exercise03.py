c398ee99a8db89ffbb36ef9bfd03b6db875788ef
endar-firmansyah <mailuntukbelajar@gmail.com>
2020-10-20 12:24:46 +0700

    Add Function Code

--- exercise03.py
+++ exercise03.py
@@ -14,17 +14,40 @@ while index < len(list):
 genap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print("Untuk bilangan genap")
 for i in range (11):
-	if i %2 == 1:
+	if i % 2 == 1:
 		continue
 	print(i)
 
 print("Untuk bilangan ganjil")
 
 for i in range ( 11):
-	if i %2 == 0:
+	if i % 2 == 0:
 		continue
 	print(i)
 
 
 
+#def bilangan (params):
+#        for bilangan in range (params):
+#        if bilangan == 0:
+#		continue
+#	if bilangan % 3 == 0:
+#	        print(bilangan)
 
+#bilangan(100)
+
+
+
+
+def bilangan3(param):
+	if param == "" or param < 3:
+		return
+
+	for bil in range(param):
+		if bil == 0:
+			continue
+
+		if bil % 3 == 0:
+			print(bil)
+
+bilangan3(100)
