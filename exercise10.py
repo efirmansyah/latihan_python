919c66bbf1f7e0785a1ea82d947ce2ca932cc8ba
endar-firmansyah <mailuntukbelajar@gmail.com>
2020-10-23 13:20:10 +0700

    Exercise10 learn about Dictionary and Tuplle

---
+++ exercise10.py
@@ -0,0 +1,41 @@
+# 10th Meeting, learn about Dictionary and Tupple
+thisdict = {
+   "brand": "Ford",
+    "model": "Mustang",
+    "year" : 1980
+}
+thisdict["year"] = 2018
+print(thisdict)
+
+
+# Try to create Dictionay on list
+phone = [{
+        "name": "Luke Skywalker",
+        "role": "Jedi",
+        "email": "luke@gmail.com",
+        },
+        {
+        "name": "Padme Amidala",
+        "role": "Princesses",
+        "email": "pade@gmail.com",
+        },
+        {
+        "name": "Han Solo",
+        "role": "Pilot",
+        "email": "solo@gmail.com",
+        }
+    ]
+
+     
+phone = {"John Doe": 2199898989, "Max Speed": 78878787}
+
+for i, v in phone.items():
+    print(f"Name: {i}, phone: {v}")
+
+
+# Learn about Tupple
+
+tp = ("Draft","Progress","Done")
+print(tp)
+
+#ls = list(tp)
